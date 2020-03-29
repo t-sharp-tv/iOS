2ba5c39afe33675679e5b8d9061448bf319039fa
t-sharp-tv <62557876+t-sharp-tv@users.noreply.github.com>
2020-03-30 01:01:42 +0900

    Update ContentView.swift

--- SampleApp/ContentView.swift
+++ SampleApp/ContentView.swift
@@ -1,42 +1,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var labelText = "Hello, swift"
-    
     var body: some View {
-        VStack {
-            VStack(spacing: 80) {
-                Text(labelText)
-                    .font(.largeTitle)
-                    .foregroundColor(Color.red)
-                Button(action: {self.labelText = "Yes Tapped"}) {
-                    Text("TAP!")
-                        .font(.footnote)
-                        .fontWeight(.thin)
-                        .foregroundColor(Color.black)
-                        .frame(width: 70,height: 20)
-                        .border(Color.gray)
-                        .background(Color.yellow)
-                        .padding(50)
-                        .background(Color.red)
-                
-                    
-                }
-                Circle()
-                    .frame(width: 150,height: 150)
-                
+        var showDetails = true
+        return VStack{
+            Button(action: {
+                showDetails.toggle()
+            }) {
+                Text("詳細表示")
             }
-            Text("Hello")
-                .background(Color.yellow)
-                .overlay(
-                    Color.green
-                        .offset(x:10,y:10))
-                .overlay(
-                    Text("swift")
-                        .background(Color.pink)
-                        .offset(x:20,y:20))
-                
             
+            if showDetails {
+                Text("詳しくはWebで！")
+                    .font(.largeTitle)
+                    .lineLimit(nil)
+            }
         }
     }
 }
